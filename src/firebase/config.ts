278f314d62fe356a1a2ff6d6fb7e51fad610169b
Shambhoo Maddheshiya <shambhoo.in@gmail.com>
2025-10-08 21:37:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4779647643-a5d80",
+  "appId": "1:123497584175:web:c218e91d85bbaa2b4cd8da",
+  "apiKey": "AIzaSyA9WXYpChziUWcFHR2W03e07-q3vgI6dxI",
+  "authDomain": "studio-4779647643-a5d80.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "123497584175"
+};
